0d5168d7614585a68aea744d6f120d3e43a11697
Damea <42821167+Damea@users.noreply.github.com>
2018-10-15 19:42:20 +0000

    Update .cc

--- .gitignore/.cc
+++ .gitignore/.cc
@@ -1,3 +1,10 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
 
 
 
@@ -6,39 +13,116 @@
 #include <fstream>
 using namespace std;
 
-
-int main()
+struct student
 {
     struct{
-    string ID;
-    string name;
-    int age;
-    char gender;
-    float score=0.0;
-    char grade;
+    string ID[5];
+    string name[5];
+    int age[5];
+    char gender[5];
+    float score[5];
+    char grade[5];
     double sum= score/5.0;
     double average;
     };
     
-    for(int i=0;i<6; i++){
-        
-    cout<<"enter your ID" <<endl 
-    cin>>s[i].ID>>endl;                    
-        
-    cout<<"enter your name:"<<endl;
-    cin>> s[i].name>>endl;
+int main()
+{
     
-    cout<<"enter your age"<<endl;
+    double totalscores=0;
+    int totalages=0;
+    
+    int malecount=0;
+    int femalecount=0;
+    int avgscore=0;
+    int studentcount=0;
+     studentcount=5;
+     
+     float score;
+     student s;
+     
+}
+    for(int i=0;i<studentcount; i++){
+        
+        
+    cout<<"enter your ID :" <<endl;
+    cin>>s.ID[i]>>endl;                    
+        
+    cout<<"\n enter your name :"<<endl;
+    cin>> s.name[i]>>endl;
+    
+    cout<<"\n enter your age :"<<endl;
     cin>>s[i].age>>endl;
     
-    cout<<"enter your score"<<endl;
-    cin>>s[i].score>>endl;
+    cout<<"\n enter your score :"<<endl;
+    cin>>s.score[i]>>endl;
     
-    cout<<"enter your grade"<<endl;
-    cin>>s[i].grade>>endl;
+    cout<<"\n enter your gender :"<<endl;
+    cin>>s.gender[i]>>endl;
+    
     
     }
+    if(s.score[i]>=80 && s.score[i]<=100)
+    {
+        s.grade[i]="A";
+    }
     
+    else if (s.score[i]>=70 && s.score[i]<80)
+    
+{
+    s.grade[i]="B";
+}
+
+    else if(s.score>=60 && s.score[i]<70)
+    {
+        s.grade[i]="C";
+    }
+    
+    else if(s.score[i]>=50 && s.score < 60)
+    {
+        s.grade[i]="D";
+    }
+    
+    else if(s.score[i]>=40 && s.score[i]<50 )
+    {
+        s.grade[i]="E";
+    }
+    
+    else 
+    {
+        s.grade[i]="F";
+    }
+    cout<<endl;
+    
+    if (s.gender[i]=="Male")
+    {
+        malecount++;
+    }
+    else if (s.gender[i]=="Female")
+    {
+        femalecount++;
+    }
+    
+    cout<<"*******************************************************************************"<<endl;
+    
+    cout<< "ID" << "  "<<"Name"<<"   "<<"Age"<<"  "<<"score"<<"   "<<"gender"<<"   "<<endl;
+    
+    cout<<"*******************************************************************************"<<endl;
+    
+    for(int i=0; i<studentcount; i++)
+    {
+        totalscores = totalscores + Eastman.score[i];
+        totalage = totalage + Eastman.score[i];
+        avgscore = totalage/studentcount;
+        avgage   = totalage/studentcount;
+        
+        student<<"The average age is --"<<endl;
+        student <<Eastman.ID[i] <<"\t"<<Eastman.Age[i]<<"\t"<< Eastern.gender[i];
+        student << endl;
+    }
+     student <<"The average score is :"<<avgage<<endl;
+        
+    }
     switch (score/10.0)
     {
     case 0:
@@ -85,4 +169,3 @@ switch (gender)
 
 return 0;
 
-}
